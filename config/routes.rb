3864bbace39d28e3da518024c75e7eafc17863a3
Herna Michel <labellehernamichel@yahoo.fr>
2019-07-05 02:43:05 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :authors
   resources :posts
 
-  resources :authors, only: [:show, :new, :create, :edit, :update]
-  resources :posts, only: [:show, :new, :create, :edit, :update]
+  # resources :authors, only: [:show, :new, :create, :edit, :update]
+  # resources :posts, only: [:show, :new, :create, :edit, :update]
 
 end
